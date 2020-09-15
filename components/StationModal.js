7ff3f9b9e14ddbefd1bca25c8760924400920ec8
Victor Yu <victoryu379@gmail.com>
2020-09-14 21:37:01 -0400

    applied station modal component into total inventory screen

--- components/StationModal.js
+++ components/StationModal.js
@@ -3,16 +3,6 @@ import { Alert, StyleSheet, Text, View, TouchableHighlight, Modal, KeyboardAvoid
 import { TextInput } from "react-native-gesture-handler"
 
 export default class StationModal extends React.Component {
-
-    static navigationOptions = ({ navigation }) => {
-		const { params = {} } = navigation.state
-		return {
-            headerShown: false,
-            headerLeft: () => null,
-            headerRight: () => null
-        }
-    }
-    
     constructor(props) {
         super(props)
         this.state = {
@@ -25,11 +15,6 @@ export default class StationModal extends React.Component {
             }
         }
     }
-    
-
-    setModalVisible(val) {
-        this.setState({modalVisible: val})
-    }
 
     updateStation(key, val) {
         if (Number(val) < 0) {
@@ -38,152 +23,133 @@ export default class StationModal extends React.Component {
         this.setState({Station: {...this.state.Station, [key]: val}})
     }
 
-    componentDidMount() {
-	
-	}
-
-	render() {
+    render() {
         return (
-                <View style={styles.centeredView}>
-                    {/* Below is the modal for pop up module */} 
-                    <Modal
-                        animationType="slide"
-                        transparent={true}
-                        visible={this.state.modalVisible}
-                        onRequestClose={() => {
-                            Alert.alert("Modal has been closed.");
-                    }}>
-                        <KeyboardAvoidingView 
-                            style={styles.centeredView}
-                            behavior="padding"
-                        >
-                            <View style={styles.modalView}>
-                                <TextInput
-                                    style={styles.sectionTitle}
-                                    onChangeText={text => this.updateStation("Name", text)}
-                                    placeholder="Enter Station Name ..."
-                                    value={this.state.Station.Name}
-                                    />
-                                
-                                <View style={styles.rowView}>
-                                    <Text 
-                                        style={{
-                                            ...styles.textStyle,
-                                            textAlign: "left",
-                                            flex: 1
-                                        }}> 
-                                        Servers:
-                                    </Text>
-                                    <TextInput
-                                        style={{
-                                            ...styles.sectionTitle,
-                                            textAlign: "right",
-                                            flex: 1
-                                        }}
-                                        onChangeText={text => this.updateStation("Servers", text)}
-                                        value={this.state.Station.Servers.toString()}
-                                        />
-                                </View>
-                                <View style={styles.rowView}>
-                                    <TouchableHighlight
-                                        style={{
-                                            ...styles.clickButton,
-                                        }}
-                                        onPress={() => { this.updateStation("Servers", (this.state.Station.Servers+1)) }}>
-                                        <Text style={styles.textStyle}> + </Text>
-                                    </TouchableHighlight>
-                                    <TouchableHighlight
-                                        style={{
-                                            ...styles.clickButton,
-                                            backgroundColor: "white",
-                                            borderColor: "#D2D2D2",
-                                            borderWidth: 1,
-                                        }}
-                                        onPress={() => { this.updateStation("Servers", (this.state.Station.Servers-1)) }}>
-                                        <Text style={styles.textStyle}> - </Text>
-                                    </TouchableHighlight>
-                                </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={this.props.visible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+            }}>
+                <KeyboardAvoidingView 
+                    style={styles.centeredView}
+                    behavior="padding"
+                >
+                    <View style={styles.modalView}>
+                        <TextInput
+                            style={styles.sectionTitle}
+                            onChangeText={text => this.updateStation("Name", text)}
+                            placeholder="Enter Station Name ..."
+                            value={this.state.Station.Name}
+                            />
+                        
+                        <View style={styles.rowView}>
+                            <Text 
+                                style={{
+                                    ...styles.textStyle,
+                                    textAlign: "left",
+                                    flex: 1
+                                }}> 
+                                Servers:
+                            </Text>
+                            <TextInput
+                                style={{
+                                    ...styles.sectionTitle,
+                                    textAlign: "right",
+                                    flex: 1
+                                }}
+                                onChangeText={text => this.updateStation("Servers", text)}
+                                value={this.state.Station.Servers.toString()}
+                                />
+                        </View>
+                        <View style={styles.rowView}>
+                            <TouchableHighlight
+                                style={{
+                                    ...styles.clickButton,
+                                }}
+                                onPress={() => { this.updateStation("Servers", (this.state.Station.Servers+1)) }}>
+                                <Text style={styles.textStyle}> + </Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight
+                                style={{
+                                    ...styles.clickButton,
+                                    backgroundColor: "white",
+                                    borderColor: "#D2D2D2",
+                                    borderWidth: 1,
+                                }}
+                                onPress={() => { this.updateStation("Servers", (this.state.Station.Servers-1)) }}>
+                                <Text style={styles.textStyle}> - </Text>
+                            </TouchableHighlight>
+                        </View>
 
-                                <View style={styles.rowView}>
-                                    <Text 
-                                        style={{
-                                            ...styles.textStyle,
-                                            textAlign: "left",
-                                            flex: 1
-                                        }}> 
-                                        Runners:
-                                    </Text>
-                                    <TextInput
-                                        style={{
-                                            ...styles.sectionTitle,
-                                            textAlign: "right",
-                                            flex: 1
-                                        }}
-                                        onChangeText={text => this.updateStation("Runners", text)}
-                                        value={this.state.Station.Runners.toString()}
-                                        />
-                                </View>
-                                <View style={styles.rowView}>
-                                    <TouchableHighlight
-                                        style={{
-                                            ...styles.clickButton,
-                                        }}
-                                        onPress={() => { this.updateStation("Runners", (this.state.Station.Runners+1)) }}>
-                                        <Text style={styles.textStyle}> + </Text>
-                                    </TouchableHighlight>
-                                    <TouchableHighlight
-                                        style={{
-                                            ...styles.clickButton,
-                                            backgroundColor: "white",
-                                            borderColor: "#D2D2D2",
-                                            borderWidth: 1,
-                                        }}
-                                        onPress={() => { this.updateStation("Runners", (this.state.Station.Runners-1)) }}>
-                                        <Text style={styles.textStyle}> - </Text>
-                                    </TouchableHighlight>
-                                </View>
+                        <View style={styles.rowView}>
+                            <Text 
+                                style={{
+                                    ...styles.textStyle,
+                                    textAlign: "left",
+                                    flex: 1
+                                }}> 
+                                Runners:
+                            </Text>
+                            <TextInput
+                                style={{
+                                    ...styles.sectionTitle,
+                                    textAlign: "right",
+                                    flex: 1
+                                }}
+                                onChangeText={text => this.updateStation("Runners", text)}
+                                value={this.state.Station.Runners.toString()}
+                                />
+                        </View>
+                        <View style={styles.rowView}>
+                            <TouchableHighlight
+                                style={{
+                                    ...styles.clickButton,
+                                }}
+                                onPress={() => { this.updateStation("Runners", (this.state.Station.Runners+1)) }}>
+                                <Text style={styles.textStyle}> + </Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight
+                                style={{
+                                    ...styles.clickButton,
+                                    backgroundColor: "white",
+                                    borderColor: "#D2D2D2",
+                                    borderWidth: 1,
+                                }}
+                                onPress={() => { this.updateStation("Runners", (this.state.Station.Runners-1)) }}>
+                                <Text style={styles.textStyle}> - </Text>
+                            </TouchableHighlight>
+                        </View>
 
-                                <Text style={styles.sectionTitle}> Details: </Text>
-                                <TextInput
-                                    style={{
-                                        ...styles.textStyle,
-                                        textAlign: "left", 
-                                        borderColor: 'gray', 
-                                        borderWidth: 1,
-                                        width: "98%",
-                                        marginTop: 5,
-                                        marginLeft: 5,
-                                        marginBottom: 10,
-                                        fontSize: 14
-                                    }}
-                                    multiline={true}
-                                    onChangeText={text => this.updateStation("Details", text)}
-                                    placeholder="Notes ..."
-                                    value={this.state.Station.Details}
-                                    />
-                                
-                                <TouchableHighlight
-                                    style={styles.openButton}
-                                    onPress={() => {
-                                        this.setModalVisible(!this.state.modalVisible);
-                                    }}>
-                                    <Text style={styles.textStyle}>Save</Text>
-                                </TouchableHighlight>
-                            </View>
-                        </KeyboardAvoidingView>
-                    </Modal>
-                    
-                    {/* Here is the button to trigger the popup */} 
-                    <TouchableHighlight
-                        style={styles.openButton}
-                        onPress={() => {
-                            this.setModalVisible(true);
-                        }}
-                        >
-                        <Text style={styles.textStyle}>Edit Station</Text>
-                    </TouchableHighlight>
-                </View>
-        )
+                        <Text style={styles.sectionTitle}> Details: </Text>
+                        <TextInput
+                            style={{
+                                ...styles.textStyle,
+                                textAlign: "left", 
+                                borderColor: 'gray', 
+                                borderWidth: 1,
+                                width: "98%",
+                                marginTop: 5,
+                                marginLeft: 5,
+                                marginBottom: 10,
+                                fontSize: 14
+                            }}
+                            multiline={true}
+                            onChangeText={text => this.updateStation("Details", text)}
+                            placeholder="Notes ..."
+                            value={this.state.Station.Details}
+                            />
+                        
+                        <TouchableHighlight
+                            style={styles.openButton}
+                            onPress={() => this.props.onSave()}>
+                            <Text style={styles.textStyle}>Save</Text>
+                        </TouchableHighlight>
+                    </View>
+                </KeyboardAvoidingView>
+            </Modal>
+        );
     }
 }
 
