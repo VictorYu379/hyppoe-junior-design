50cab755d32c6e280bc205db1a7792013d171dd0
Jiawen Ge <jiawen_kevin_ge@gatech.edu>
2020-09-16 17:44:37 -0400

    Added runner alerts screen

--- screens/RunnerHistoryScreen.js
+++ screens/RunnerHistoryScreen.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import ShadowedBox from '../components/ShadowedBox';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function RunnerHistoryScreen({ navigation }) {
 
 	const taskList = [
-		{key: 1, item: 'Sweetwater', from: 'Inventory', to: 'Sta. 1 Big Tent', status: 'Not Picked Up'},
+		{key: 1, item: 'Sweetwater', from: 'Inventory', to: 'Sta. 1 Big Tent', status: 'Unstarted'},
 		{key: 2, item: 'Coors Light', from: 'Inventory', to: 'Sta. 1 Big Tent', status: 'Pending'},
 		{key: 3, item: 'Bud Light', from: 'Inventory', to: 'Sta. 1 Big Tent', status: 'Complete'},
 	]
@@ -14,10 +15,10 @@ export default function RunnerHistoryScreen({ navigation }) {
 
 	const textColor = (text) => {
 		if (text == 'Complete') {
-			return '51A760'
+			return '#51A760'
 		} else if (text == 'Pending') {
 			return '#E9C041'
-		} else if (text == 'Not Picked Up') {
+		} else if (text == 'Unstarted') {
 			return '#CD594A'
 		}
 		return 'black'
@@ -28,11 +29,17 @@ export default function RunnerHistoryScreen({ navigation }) {
 			<View style={styles.rowView}>
 				<View style={{
 					flexDirection: 'column',
+					flex: 7,
 				}}>
-					<Text style={styles.rowTitle}> {task.item} </Text>
-					<Text style={styles.rowText}> {task.from} to {task.to} </Text>
+					<Text style={styles.rowTitle}>{task.item}</Text>
+					<Text style={styles.rowText}>{task.from} to {task.to}</Text>
 				</View>
-				<Text style={{...styles.rowTitle, color: textColor(task.status)}}> {task.status} </Text>
+				<Text style={{
+					...styles.rowTitle, 
+					color: textColor(task.status),
+					textAlign: "right",
+					flex: 3,
+					}}>{task.status}</Text>
 			</View>
 		);
 	});
@@ -40,19 +47,14 @@ export default function RunnerHistoryScreen({ navigation }) {
 	return (
 		<View style={styles.container}>
 			<ShadowedBox width={'90%'} margin={10}>
-				<View style={{
-					width: '90%',
-					marginTop: 20,
-					marginBottom: 10
-				}}>
-					<Text style={styles.sectionTitle}> Runner {runner}: Task History </Text>
+				<View style={{width: '90%', marginTop: 25, marginBottom: 10}}>
+					<Text style={styles.sectionTitle}>Runner {runner}: Task History</Text>
 				</View>
-				<View style={{
-					width: '90%',
-					marginBottom: 25
-				}}>
-					{taskHistory}
-				</View>
+				<View style = {{width: '90%', marginBottom: 25, maxHeight: '85%'}}>
+                    <ScrollView>
+                        {taskHistory}
+                    </ScrollView>
+                </View>
 			</ShadowedBox>
 		</View>
 	);
@@ -72,7 +74,8 @@ const styles = StyleSheet.create({
 		alignItems: "flex-end",
 		borderBottomColor: "grey",
         borderBottomWidth: StyleSheet.hairlineWidth,
-        padding: 5
+		padding: 5,
+		flex: 1,
 	},
 	sectionTitle: {
         color: "black",
@@ -87,13 +90,17 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: "bold",
 		textAlign: "left",
-		marginBottom: 2
+		marginBottom: 2,
+		flexWrap: "wrap",
+		width: "100%"
 	},
 	rowText: {
         color: "#5C5A5A",
         fontFamily: "Arial",
 		fontSize: 16,
 		fontWeight: "normal",
-        textAlign: "left",
+		textAlign: "left",
+		flexWrap: "wrap",
+		width: "100%"
     }
 });
