ee748baec90dda88e15799b9ebddfad7de3e49f6
Michael Qian <609945177@qq.com>
2020-09-17 17:48:39 +0800

    finished runner Task&Dashboard page

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import { AppLoading } from 'expo';
 import DummyScreen from './screens/DummyScreen';
 import TotalInventoryStationOverviewScreen from './screens/TotalInventoryStationOverviewScreen';
 import RunnerStationInventoryScreen from './screens/RunnerStationInventoryScreen';
+import RunnerDashBoardScreen from './screens/RunnerDashBoardScreen';
+import RunnerTaskScreen from './screens/RunnerTaskScreen';
 import HyppoeHeader from './components/HyppoeHeader';
 
 const Stack = createStackNavigator();
@@ -28,7 +30,7 @@ export default function App() {
 						<Stack.Screen name="Dummy" component={DummyScreen} />
 						<Stack.Screen
 							name="Total Inventory"
-							component={RunnerStationInventoryScreen}
+							component={RunnerTaskScreen}
 							options={{
 								header: ({ navigation, props }) => {
 									return <HyppoeHeader navigation={navigation} mode="full"/>;
