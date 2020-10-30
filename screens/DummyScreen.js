652abe272be0ca3fdffdc83f939217afdf67ae01
Victor Yu <victoryu379@gmail.com>
2020-10-30 15:21:16 -0400

    small fix

--- screens/DummyScreen.js
+++ screens/DummyScreen.js
@@ -20,11 +20,7 @@ export default function DummyScreen({ navigation }) {
 	return (
 		<View style={styles.container}>
 			<TouchableOpacity
-<<<<<<< HEAD
 				onPress={() => navigation.navigate('Manager Dashboard')}>
-=======
-				onPress={() => navigation.navigate('Manager Return Inventory')}>
->>>>>>> master
 				<Text>Open up App.js to start working on your app!</Text>
 			</TouchableOpacity>
 			<StatusBar style="auto" />
